91a9d7aab939b1e66c090e316b090080fb42bf93
Ксения Быстрова <ksenya_bystrova@mail.ru>
2020-03-07 12:14:40 +0500

    Add draft interface and file selection functionality

---
+++ SyllabusChecker/InputData.cs
@@ -0,0 +1,10 @@
+﻿namespace SyllabusChecker
+{
+    //Для хранения и передачи путей к файлам, выбранных в главном окне
+    public class InputData
+    {
+        public string ModelPath { get; set; }
+        public string SyllablePath { get; set; }
+        public string ResultFolderPath { get; set; }
+    }
+}
--- SyllabusChecker/MainWindow.xaml.cs
+++ SyllabusChecker/MainWindow.xaml.cs
@@ -1,28 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
 
 namespace SyllabusChecker
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        private readonly InputData InputData;
+
         public MainWindow()
         {
             InitializeComponent();
+            InputData = new InputData();
+        }
+
+        //Обработка нажатия на кнопку выбора расположения файла с макетом
+        private void BtnSelectModelPath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Doc or Docx files|*.doc;*.docx"
+            };
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                InputData.ModelPath = ofd.FileName;
+                TbModelPath.Text = ofd.FileName;
+            }
+        }
+
+        //Обработка нажатия на кнопку выбора расположения готового файла рабочей программы
+        private void BtnSelectSyllablePath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Doc or Docx files|*.doc;*.docx"
+            };
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                InputData.SyllablePath = ofd.FileName;
+                TbSyllablePath.Text = ofd.FileName;
+            }
+        }
+
+        //Обработка нажатия на кнопку выбора расположения проверенного файла рабочей программы
+        private void BtnSelectResultFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog
+            {
+                RootFolder = Environment.SpecialFolder.Desktop
+            };
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                InputData.ResultFolderPath = fbd.SelectedPath;
+                TbResultFolderPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        //Обработка нажатия на кнопку "Выполнить проверку"
+        private void BtnCheckSyllableStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputData.ModelPath == null)
+            {
+                MessageBox.Show("Не выбран файл модели рабочей программы!");
+                return;
+            }
+            else if (InputData.SyllablePath == null)
+            {
+                MessageBox.Show("Не выбран проверяемый файл рабочей программы!");
+                return;
+            }
+            else if (InputData.ResultFolderPath == null)
+            {
+                MessageBox.Show("Не выбран путь для сохранения результата!");
+                return;
+            }
+
+            //Здесь -- запуск проверки
         }
     }
 }
